e1dd9f1ebd5d4af896ae150dfc79ccca06087d9e
Praveen Kumar <pk46066@gmail.com>
2025-01-06 16:59:32 +0530

    create user schema

---
+++ models/userModel.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+  first_name: {
+    type: String,
+    required: [true, 'First name is required'],
+    trim: true,
+    lowercase: true,
+    minlength: [3, 'First name must be at least 3 characters long'],
+    maxlength: [50, 'First name cannot exceed 50 characters']
+  },
+  middle_name: {
+    type: String,
+    required: [false, 'Middle name is required'],
+    trim: true,
+    lowercase: true,
+    minlength: [3, 'Middle name must be at least 3 characters long'],
+    maxlength: [50, 'Middle name cannot exceed 50 characters']
+  },
+  last_name: {
+    type: String,
+    required: [false, 'Last name is required'],
+    trim: true,
+    lowercase: true,
+    minlength: [3, 'Last name must be at least 3 characters long'],
+    maxlength: [50, 'Last name cannot exceed 50 characters']
+  },
+  phone: {
+    type: String,
+    required: [true, 'Phone number is required'],
+    match: [/^\d{10}$/, 'Please provide a valid 10-digit phone number'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    uniqueCaseInsensitive: true,
+    validate: validateEmail,
+    match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        'Please fill a valid email address'
+    ]
+  },
+  role: {
+    type: String,
+    default: "user",
+    enum: ['admin', 'user'],
+    required: true
+  },
+  date_Of_birth: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters long']
+  },
+  is_admin: {
+    type: Boolean,
+    default: false
+  },
+  status: {
+    type: String,
+    default: "Inactive",
+    enum: {
+      values: ['Active', 'Inactive'],
+      message: '{VALUE} is not a valid status'
+    }
+  },
+  is_deleted:{
+    type:Boolean,
+    default:false
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: Date
+});
+
+async function validateEmail(email) {
+  if (!validator.isEmail(email)) throw new Error("Please enter a valid email address.")
+  const user = await userModel.findOne({ email })
+  if (user) throw new Error("A user is already registered with this email address.")
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
